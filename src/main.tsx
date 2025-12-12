40fa1c27ff1efcf63a01a531dc316ea0d6979276
Jing Chen <xiayw_1@126.com>
2025-12-12 17:18:10 +0800

    start to next layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import router from "./routing/routes";
 
 const MINUTE = 1000 * 60;
 const queryClient = new QueryClient({
@@ -23,8 +24,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
-
+        <RouterProvider router={router}></RouterProvider>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </React.StrictMode>
